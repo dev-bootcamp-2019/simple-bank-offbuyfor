c8c72b9a4f58d6bcd2f4cddfef5ba4a566ed3322
offbuyfor <43789207+offbuyfor@users.noreply.github.com>
2019-01-08 12:20:20 -0500

    Update SimpleBank.sol

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -59,8 +59,8 @@ contract SimpleBank {
     /// @return The users enrolled status
     // Emit the appropriate event
     function enroll() public returns (bool){
-          require(enrolled[msg.sender]!=true, "sender already enrolled");
-          enrolled[msg.sender] = true;
+        //require(enrolled[msg.sender]!=true,"sender already enrolled");
+        enrolled[msg.sender] = true;
         balances[msg.sender] = 0;
         emit LogEnrolled (msg.sender);
         return true;
@@ -74,8 +74,8 @@ contract SimpleBank {
     function deposit() external payable returns (uint) {
         /* Add the amount to the user's balance, call the event associated with a deposit,
           then return the balance of the user */
-        require(enrolled[msg.sender], "sender is not enrolled");
-               balances[msg.sender] += msg.value;
+        require(enrolled[msg.sender] == true);
+        balances[msg.sender] += msg.value;
         emit LogDepositMade(msg.sender, msg.value);
         return balances[msg.sender];
     }
@@ -90,8 +90,8 @@ contract SimpleBank {
            Subtract the amount from the sender's balance, and try to send that amount of ether
            to the user attempting to withdraw. 
            return the user's balance.*/
-        require(enrolled[msg.sender], "sender is not enrolled");
-        require(balances[msg.sender] >= withdrawAmount, "Insufficient Funds");
+        require(enrolled[msg.sender]==true);
+        require(balances[msg.sender] >= withdrawAmount);
         balances[msg.sender] -= withdrawAmount;
         emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
         return balances[msg.sender];
@@ -102,7 +102,7 @@ contract SimpleBank {
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract
-    function() {
+    function() external {
         revert();
     }
 }
